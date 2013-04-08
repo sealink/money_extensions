38403a98a506edcf3bfd43f246add2b853e74240
Michael Noack <michael@noack.com.au>
2013-04-08 16:12:18 +0930

    Use simple methods instead of composed_of * Enables use of money_field with calculated attributes (e.g. nett) * Remove options (db_field never used) * Move money fields near money_field

--- lib/money_extensions/money_field.rb
+++ lib/money_extensions/money_field.rb
@@ -49,16 +49,25 @@ module MoneyField
 
     # Add a money field attribute
     #
-    # By default it will use attribute_in_cents as db field, but this can
-    # be overridden by specifying :db_field => 'somthing_else'
-    def money_field(attribute, options={})
-      db_field = options[:db_field] || attribute.to_s + '_in_cents'
-      self.composed_of(attribute,
-        :class_name => "Money",
-        :allow_nil  => true,
-        :mapping    => [[db_field, 'cents']],
-        :converter  => Proc.new {|field| field.to_money}
-      )
+    # By default it will use attribute_in_cents for cents value
+    def money_field(attribute)
+      module_eval <<-METHOD
+        def #{attribute}
+          Money.new(#{attribute}_in_cents) if #{attribute}_in_cents.present?
+        end
+
+        # Allow assigning of non money objects directly
+        # Allows form data to be directly passed through
+        # e.g. object.cost = '5.1'
+        def #{attribute}=(money)
+          self.#{attribute}_in_cents = money.try(:to_money).try(:cents)
+        end
+      METHOD
+    end
+
+
+    def money_fields(*attributes)
+      attributes.each {|a| self.money_field(a)}
     end
 
 
@@ -70,12 +79,6 @@ module MoneyField
           end
         METHOD
       end
-    end
-
-
-    def money_fields(*attributes)
-      opts = attributes.extract_options!
-      attributes.each {|a| self.money_field(a, opts)}
     end
   end
 end
