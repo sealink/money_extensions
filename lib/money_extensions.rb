80c245d5189cbce16b872422741357b4baec3be2
john.mortlock <john.mortlock@gmail.com>
2022-07-01 11:52:07 +0930

    Move money_field as its also part of the AR extension

--- lib/money_extensions.rb
+++ lib/money_extensions.rb
@@ -4,12 +4,12 @@ module MoneyExtensions
   require 'money_extensions/integer'
   require 'money_extensions/split_between'
   require 'money_extensions/html_format'
-  require 'money_extensions/money_field'
   require 'money_extensions/numeric'
   require 'money_extensions/string'
 
   # TODO: Defer requiring the active record extension to who needs it
   if Module.const_defined?('ActiveRecord')
+    require 'money_extensions/active_record/money_field'
     require 'money_extensions/active_record/extensions'
   end
 
